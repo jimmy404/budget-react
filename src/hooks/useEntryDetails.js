4b584ae1b4ccfdc80619839ec96dffc7b24b6b24
Jimmy404 <bruno_pier@hotmail.com>
2022-05-25 14:53:54 -0300

    feat: adds custom hook

---
+++ src/hooks/useEntryDetails.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+
+import { addEntryRedux, updateEntryRedux } from "../actions/entries.actions";
+import { closeEditModal } from "../actions/modals.action";
+
+const useEntryDetails = (desc = "", val = "", isExp = true) => {
+  const [description, setDescription] = useState(desc);
+  const [value, setValue] = useState(val);
+  const [isExpense, setIsExpense] = useState(isExp);
+  const dispatch = useDispatch();
+
+  const resetValues = () => {
+    setDescription("");
+    setValue("");
+    setIsExpense(true);
+  };
+
+  useEffect(() => {
+    setDescription(desc);
+    setValue(val);
+    setIsExpense(isExp);
+  }, [desc, val, isExp]);
+
+  const updateEntry = (id) => {
+    dispatch(
+      updateEntryRedux(id, {
+        id,
+        description,
+        value,
+        isExpense,
+      })
+    );
+    dispatch(closeEditModal());
+    resetValues();
+  };
+
+  const addEntry = () => {
+    dispatch(
+      addEntryRedux({
+        id: uuidv4(),
+        description,
+        value,
+        isExpense,
+      })
+    );
+    resetValues();
+  };
+
+  return {
+    description,
+    setDescription,
+    value,
+    setValue,
+    isExpense,
+    setIsExpense,
+    addEntry,
+    updateEntry,
+  };
+};
+
+export default useEntryDetails;
