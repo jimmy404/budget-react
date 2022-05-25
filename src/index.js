033a016b2d702980a78b5269059882a73100f172
Jimmy404 <bruno_pier@hotmail.com>
2022-05-24 23:02:20 -0300

    feat: implements Provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { Provider } from "react-redux";
 import App from "./App";
+
+import "./index.css";
 import "semantic-ui-css/semantic.min.css";
 
+import storeConfig from "./store/configureStore";
+
+const store = storeConfig();
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
